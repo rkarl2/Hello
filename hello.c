551a1b1560e6d046635ff338020c5ebf413b8087
rkarl2 <59965479+rkarl2@users.noreply.github.com>
2020-01-16 08:16:45 -0600

    File

--- hello.c
+++ hello.c
@@ -3,6 +3,13 @@ Author:Ryan Karl
 
 1/16/2020
 Collaborators: None
-Purpose: print hello
+Purpose: print hello and major
 */
- 
+ #include <stdio.h>
+
+ int main (void){
+
+   printf("Hello World\n");
+   printf("Electrical engineer major\n");
+
+ }
