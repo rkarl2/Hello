c2256a551b5be517996a3c3668f45998f3fa04fd
rkarl2 <59965479+rkarl2@users.noreply.github.com>
2020-01-16 21:30:13 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,15 +2,19 @@
 Author:Ryan Karl
 1/16/2020
 Collaborators: Amy Wolterman
-Purpose: print hello and major
+Purpose: Print name and major
 */
  #include <stdio.h>
 
  int main (void){
 
    printf("Ryan Karl\n");
+
    printf("Electrical engineer major\n");
+
    printf("Amy Wolterman\n");
+
    printf("Mechanical Engineering\n");
 
+      return 0;
  }
